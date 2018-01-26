677846226bc5a6d5e1377e06f1b6cdcb65138cd3
Matt Pearson <mattpearson@gmail.com>
2018-01-26 17:37:46 -0600

    Example of how to withdraw crypto from GDAX exchange without using 3rd party API.
    Example of how to withdraw crypto from GDAX exchange without using 3rd party API.

--- GDAX Withdraw Crypto.py
+++ GDAX Withdraw Crypto.py
@@ -1,4 +1,4 @@
-## Example of how to withdraw crypto from GDAX exchange without using 3rd party API.
+## Example of how to withdraw crypto from GDAX exchange without using 3rd party API. 
 
 import json, hmac, hashlib, time, requests, base64
 from requests.auth import AuthBase
